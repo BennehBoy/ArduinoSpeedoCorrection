17cdd931a12a7c9e3896c3eb077b9b55d74fb6e6
Ben Anderson <bennyboy@benneh.net>
2017-01-19 14:29:15 +0000

    Added 1hz baseline output for stationary vehicle

--- ArduinoSpeedoCorrection.ino
+++ ArduinoSpeedoCorrection.ino
@@ -1,63 +1,74 @@
-//1986 Porsche 944 Turbo Speedometer Calibrator
+//ArduinoSpeedoCorrection by Ben Anderson
+//Version 0.2
+//
+//Based upon 1986 Porsche 944 Turbo Speedometer Calibrator by Tom M'Guinness - http://members.rennlist.com/tom86951/Speedometer%20Calibrator%20Page1.html
 //Public Domain
-//raw 944 transmission sensor signal must be processed by external schmitt trigger
-//Schmitt trigger should bypass filer and pull-up resistor
-//Use LM2940-10 with decoupling caps per datasheet to power Arduino in automotive setting
 //Arduino Pin 10 supplies modified signal for speedometer
 //
 //Modified to accept a digital signal on pin 3 rather than a ground based pulse - requires 10K pulldown
- 
-const int debounce = 2500;
-const int speedometerPin = 10;                    
-const int sensorPin = 3;                                     
-int pulseState = HIGH;                                        
-volatile unsigned long currentMicros = 0; 
-volatile unsigned long previousMicros= 0;  
-volatile unsigned long currentSpeed = 0;  
+
+const int debounce = 2500; // this may need to reduce for TD5 - there are some scenarios where a signal may only be 2.5ms
+const int speedometerPin = 10;
+const int sensorPin = 3;
+int pulseState = HIGH;
+volatile unsigned long currentMicros = 0; // we treat these as volatile because they can be changed by our interrupt
+volatile unsigned long previousMicros = 0;
+volatile unsigned long currentSpeed = 0;
 volatile unsigned long previousSpeed = 0;
-volatile unsigned long interval = 0;                 
-unsigned long modInterval = 0;                
-float calFactor = 1.1;   // decrease to slow down speedometer                           
-                                     // calFactor of 1 makes no change to speedometer
- 
+volatile unsigned long interval = 0;
+volatile unsigned long oneHertz = 0;
+unsigned long modInterval = 0;
+float calFactor = 1.1;   // decrease to slow down speedometer  (use 1/ratio to get calfactors for speedup)
+// calFactor of 1 makes no change to speedometer
+
 void setup()
 {
-  Serial.begin(115200);
-pinMode (13, OUTPUT);                        
-pinMode(speedometerPin, OUTPUT);    
-pinMode(sensorPin, INPUT);                 
-digitalWrite (sensorPin, LOW);             
-attachInterrupt (1, iSr, RISING);        
+  Serial.begin(115200); //for debug - needs to be a high baud otherwise we delay the actuial signal measurement
+  pinMode(speedometerPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  digitalWrite (sensorPin, LOW); // pin 10 is pulled low by a 10k resistor.
+  attachInterrupt (1, iSr, RISING); // when we detect a pulse call our interrupt code
 }
- 
+
 void loop()
 {
-  noInterrupts();                                                 
-  modInterval=interval;                                    
-  interrupts();                                                      
-  currentMicros = micros();                             
-  if (currentMicros-previousSpeed<1000000) 
-  {
-   if (currentMicros - previousMicros>((modInterval/2)/calFactor))          
-       { previousMicros = currentMicros;                                                    
-        if (pulseState == LOW) pulseState = HIGH; else pulseState = LOW;
-        digitalWrite(13, pulseState);         //to blink onboard LED  
-        Serial.println(micros());                                                        
-        digitalWrite(speedometerPin, pulseState);
-        }                                  
-   }
+  noInterrupts();
+  modInterval = interval;
+  interrupts();
+  currentMicros = micros();
+  if (currentMicros - previousSpeed < 300000) // slowest moving signal from SLABS should be 454micros peak to peak, so if we are seeing much more than half of this then we must be stationary
+  { // 1mph = 2.2hz ~230ms, so anything above this means we are stationary
+    if (currentMicros - previousMicros > ((modInterval / 2) / calFactor)) //divided by 2 because modInterval is one whole wavelength, we need to do a high low transition for half the time each of this.
+    { previousMicros = currentMicros;
+      if (pulseState == LOW) pulseState = HIGH; else pulseState = LOW;
+      Serial.println(micros()); //debug out
+      digitalWrite(speedometerPin, pulseState); // output our corrected VSS
+      oneHertz = currentMicros;
+    }
+  }
+
+  if (currentMicros - oneHertz > 500000) { // if we are stationary output a 1hz signal
+    if (pulseState == LOW) {
+      pulseState = HIGH;
+    } else {
+      pulseState = LOW;
+    }
+    digitalWrite(speedometerPin, pulseState); // output our one hz VSS
+    oneHertz = currentMicros;
+  }
+
 }
- 
+
 void iSr()
 {
-  currentSpeed=micros();                   
-  if (digitalRead(sensorPin)==HIGH) 
-     {
-        if ((currentSpeed - previousSpeed) > debounce)  
-           {
-            interval = currentSpeed - previousSpeed;        
-            previousSpeed=currentSpeed;
-            //Serial.println(interval);                        
-           }
-      }
+  currentSpeed = micros();
+  if (digitalRead(sensorPin) == HIGH)
+  {
+    if ((currentSpeed - previousSpeed) > debounce)
+    {
+      interval = currentSpeed - previousSpeed; // work out the length of time our signal stays high
+      previousSpeed = currentSpeed;
+      //Serial.println(interval);
+    }
+  }
 }
--- TestHarness/TestHarness.ino
+++ TestHarness/TestHarness.ino
@@ -1,8 +1,7 @@
 // Simple waveform generator based upon blink
 
-int interval = 2; // this is the time in milliseconds that we want our signal to be high and then low
-                  // a value of 2 gives a waveform repeat of 4ms or 250hz
-                  // TD5 Land Rover SLABS output is baseline 1hz when stationary, and vehicle speed * 2.2 in hz (8000 pulses per miles)
+int interval = 230; // this is the time in milliseconds that we want our signal to be high and then low
+                    // for example a value of 2 gives a waveform repeat opf 4ms or 250hz ~114mph (unlikely in a TD5!)
 
 void setup() {
   // initialize digital pin 12 as an output.
